2681f8c168465437d91072a3c932b49a164803bc
Leighton A Pitman <leightonpitman14@gmail.com>
2020-02-19 19:45:18 -0500

    init commit

---
+++ db.js
@@ -0,0 +1,34 @@
+const pg = require('pg');
+
+const sync = async () => {
+  //DROP and RECREATE TABLES
+  //remember "departmentId" will need to be in quotes
+};
+
+const readDepartments = async () => {
+  return [];
+};
+
+const readUsers = async () => {
+  return [];
+};
+
+module.exports = {
+  sync,
+  readDepartments,
+  readUsers
+};
+//you will eventually need to export all of these
+/*
+module.exports = {
+  sync,
+  readDepartments,
+  readUsers,
+  createDepartment,
+  createUser,
+  deleteDepartment,
+  deleteUser,
+  updateUser,
+  updateDepartment
+};
+*/
